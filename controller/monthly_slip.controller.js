d581270bf3a54b3608516e7b04e5a90563cb8758
Elonnativesystem <ponnumani@elonnativesystem.com>
2024-11-25 10:12:58 +0530

    2

--- controller/monthly_slip.controller.js
+++ controller/monthly_slip.controller.js
@@ -1,43 +1,11 @@
 const slip = require('../model/monthly_slip.model.js')
 
 
-// exports.create = async(req, res) =>{
-//    try {
-//     const filePath = path.join(__dirname, '../views/slip.ejs');
-//     const htmlContent = fs.readFileSync(filePath, 'utf8');
-//     const buffer = await generatePDF(htmlContent)
-//     const base64Data = buffer.toString('base64');
-//     // const base64Encoded = Buffer.from(htmlContent).toString('base64');
-
-//     const {employeeId, month, year} = req.body
-//     const check = await slip.findOne({
-//         $and: [
-//             { employeeId },   // Match name
-//             { month } ,
-//             { year }       // Match age
-//         ]
-//     })
-//     if(check){
-//        return  res.json({message:"already resister this month"})
-//     }
-//         const paySlip = await new slip({
-//             employeeId,
-//             month,
-//             year,
-//             file:base64Data
-//         })
-//         const result = await paySlip.save()
-//         res.json(result)
-
-//    } catch (error) {
-//     console.error('Error storing payslip:', error);
-//    }
-// }
 
 
 exports.getSlip = async (req, res) => {
     try {
-        const { employeeId, month, year } = req.body
+        const { employeeId, month, year } = req.query
         const paySlipRetrieve = await slip.findOne({ employeeId, month, year });
 
         if (!paySlipRetrieve) {
